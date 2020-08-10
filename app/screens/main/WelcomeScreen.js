eb8aa8b2b13b042cc481e9eee172e7de2035bc4c
theafolayan <theafolayan@gmail.com>
2020-08-10 19:53:43 +0100

    added register and login buttons to WelcomeScreen.js in main folder

--- app/screens/main/WelcomeScreen.js
+++ app/screens/main/WelcomeScreen.js
@@ -4,13 +4,25 @@ import { StyleSheet, Text, View, ImageBackground } from 'react-native'
 export default function WelcomeScreen(props) {
     return (
         <ImageBackground source={require('../../../assets/background.jpg')} style={styles.background}> 
-
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
         </ImageBackground>
     )
 }
 
 const styles = StyleSheet.create({
     background: {
-        flex: 1
+        flex: 1,
+        justifyContent: "flex-end"
+    },
+    loginButton: {
+        height: 70,
+        width: "100%",
+        backgroundColor: "#fc5c65"
+    },
+    registerButton: {
+        width: "100%",
+        height: 70,
+        backgroundColor: "#4ecdc4"
     }
 })
