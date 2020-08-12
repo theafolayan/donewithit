5f8cc74ce29ce183c81e678057431ecb52ec10cc
theafolayan <theafolayan@gmail.com>
2020-08-12 17:49:05 +0100

    finished layout for ViewImageScreen in main fodler

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, SafeAreaView, ImageBackground, Dimensions, TouchableOpacity } from 'react-native';
 // import WelcomeScreen from './app/screens/WelcomeScreen'
 import WelcomeScreen from './app/screens/main/WelcomeScreen'
-import ViewImageScreen from './app/screens/ViewImageScreen'
+import ViewImageScreen from './app/screens/main/ViewImageScreen'
 
 export default function App() {
   return (
     //App root lives here 
-      <WelcomeScreen></WelcomeScreen>
+      // <WelcomeScreen></WelcomeScreen>
+    <ViewImageScreen/>
    
   );
 }
