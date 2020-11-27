456a911a484fe66a0344c71179b660abd852b6d8
theafolayan <theafolayan@gmail.com>
2020-11-27 07:51:03 +0100

    replaced generic closeIcon with proper MaterialCommunityIcon

--- app/screens/main/ViewImageScreen.js
+++ app/screens/main/ViewImageScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../../config/colors";
 export default function ViewImageScreen() {
     return (
         <View style={styles.container}> 
-            <View style={styles.closeIcon}></View>
+            <View style={styles.closeIcon}>
+                <MaterialCommunityIcons name="close" color="white" size={ 30}/>
+            </View>
             <View style={styles.deleteIcon}></View>
             <Image resizeMode="contain" style={styles.image} source={require("../../../assets/chair.jpg")}/>
         </View>
@@ -15,9 +18,6 @@ export default function ViewImageScreen() {
 
 const styles = StyleSheet.create({
     closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.primary,
         position: "absolute",
         top: 40,
         left: 30
