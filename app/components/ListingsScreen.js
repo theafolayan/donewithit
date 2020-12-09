edbebe3fb3067bb0df7510aaae0e2d4ebac9985a
theafolayan <theafolayan@gmail.com>
2020-12-09 14:22:07 +0100

    created ListingsScreen

---
+++ app/components/ListingsScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function ListingsScreen() {
+    return (
+        <Screen> 
+            
+       </Screen>
+    )
+}
